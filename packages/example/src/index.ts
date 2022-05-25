15d94e5fe2eae5e7f33efee7ef7460007158908c
Stan Chang Khin Boon <khinboon@gmail.com>
2022-05-25 18:52:44 +0800

    build(deps): Upgrade to v0.3.0

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -3,6 +3,7 @@
 // simple-import-sort/imports rule
 import goodbye from './goodbye';
 import hello from './hello';
+import type { HumanDesc } from './Human';
 
 // eqeqeq rule, always for all, never for null
 if (hello == null) {
@@ -17,6 +18,10 @@ function reassignParam(draft: ReassignParamOptions): void {
   draft.value = 'true';
 }
 reassignParam({});
+
+export function printHumanDesc(humanDesc: HumanDesc) {
+  console.log(humanDesc);
+}
 
 if (hello != null) {
   console.log('TRUE');
